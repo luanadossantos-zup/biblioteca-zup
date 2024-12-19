3bc77e1eabf9465d58d3cdbfdb32c7050b2e81fe
Luana Silva <luana.dossantos@zup.com.br>
2024-12-19 10:10:32 -0300

    fix: conserta erro do while

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int escolhaUsuarioMenu;
-
-        escolhaUsuarioMenu = menuDeBoasVindas();
-        exibeProgramaEscolhidoMenu(escolhaUsuarioMenu);
-
+        menuDeBoasVindas();
 
 
     }
 
 
-    public static int  menuDeBoasVindas() {
+    public static void menuDeBoasVindas() {
         Scanner scanner = new Scanner(System.in);
         int escolhaUsuarioMenu = 0;
 
+
         do {
+            System.out.println(" ");
             System.out.println("Bem-vindo ao Sistema de Gerenciamento de Biblioteca Zup!");
             System.out.println("Escolha uma opção:");
             System.out.println("1 - Cadastrar livro");
@@ -31,11 +29,13 @@ public class Main {
 
             try {
                 escolhaUsuarioMenu = scanner.nextInt();
+                scanner.nextLine();
 
-                if (escolhaUsuarioMenu > 6 || escolhaUsuarioMenu <0) {
+                if (escolhaUsuarioMenu > 6 || escolhaUsuarioMenu < 0) {
                     System.out.println("-------------------------------------");
                     System.out.println("Por favor, apenas números de 1 a 6!");
                     System.out.println("-------------------------------------");
+                    continue;
                 }
 
             } catch (Exception e) {
@@ -43,98 +43,99 @@ public class Main {
                 System.out.println("Por favor, apenas números de 1 a 6!");
                 System.out.println("-------------------------------------");
                 scanner.next();
+                continue;
             }
-        } while (escolhaUsuarioMenu > 6 || escolhaUsuarioMenu < 0);
 
 
-        return escolhaUsuarioMenu;
-
-    }
-
-
-    public static void exibeProgramaEscolhidoMenu(int escolhaUsuario) {
-        Scanner scanner = new Scanner(System.in);
-        Biblioteca biblioteca = new Biblioteca();
-        Usuario usuario = new Usuario();
-        Livro livro = new Livro();
-        String nomeDoLivro;
-        String autorDoLivro;
-        String isbnCadastro;
-        String nomeDoUsuario;
-        int idCadastroDoUsuario;
-        int idEmprestimoUsuario;
-        String isbnEmprestimo;
-
-        switch (escolhaUsuario) {
-
-            case 1 :
-                //Cadastro de livro
-                System.out.println("Digite o título do livro:");
-                nomeDoLivro = scanner.nextLine();
-                System.out.println("Digite o autor do livro:");
-                autorDoLivro = scanner.nextLine();
-                System.out.println("Digite o ISBN do livro:");
-                isbnCadastro = scanner.nextLine();
-
-                livro.setTitulo(nomeDoLivro);
-                livro.setAutor(autorDoLivro);
-                livro.setIsbn(isbnCadastro);
-                livro.setDisponivel(true);
-
-                biblioteca.cadastrarLivro(livro);
-                scanner.close();
+            Biblioteca biblioteca = new Biblioteca();
+            Usuario usuario = new Usuario();
+            Livro livro = new Livro();
+            String nomeDoLivro;
+            String autorDoLivro;
+            String isbnCadastro;
+            String nomeDoUsuario;
+            int idCadastroDoUsuario;
+            int idEmprestimoUsuario;
+            String isbnEmprestimo;
 
 
-                break;
-
-            case 2 :
-                //Cadastro de usuário
-                System.out.println("Digite o nome do usuário:");
-                nomeDoUsuario = scanner.nextLine();
-                System.out.println("Digite ID do usuário:");
-                idCadastroDoUsuario = scanner.nextInt();
-                usuario.setNome(nomeDoUsuario);
-                usuario.setId(idCadastroDoUsuario);
-
-                biblioteca.cadastrarUsuario(usuario);
-                scanner.close();
-                break;
-
-            case 3:
-                //Realizar empréstimo de livro
-                System.out.println("Digite o ISBN do livro:");
-                isbnEmprestimo = scanner.nextLine();
-                System.out.println("Digite o ID do usuario");
-                idEmprestimoUsuario = scanner.nextInt();
+            switch (escolhaUsuarioMenu) {
 
 
+                case 1:
+                    //Cadastro de livro
 
-                for (int i = 0; i < biblioteca.getLivros().size();i++) {
-                    if (biblioteca.getLivros().get(i).getIsbn().equals(isbnEmprestimo)) {
+                    System.out.println("Digite o título do livro:");
+                    nomeDoLivro = scanner.nextLine();
 
-                        biblioteca.realizarEmprestimo(isbnEmprestimo, idEmprestimoUsuario);
+                    System.out.println("Digite o autor do livro:");
+                    autorDoLivro = scanner.nextLine();
+
+                    System.out.println("Digite o ISBN do livro:");
+                    isbnCadastro = scanner.nextLine();
 
 
+                    livro.setTitulo(nomeDoLivro);
+                    livro.setAutor(autorDoLivro);
+                    livro.setIsbn(isbnCadastro);
+                    livro.setDisponivel(true);
 
+                    biblioteca.cadastrarLivro(livro);
+
+
+                    break;
+
+                case 2:
+                    //Cadastro de usuário
+                    System.out.println("Digite o nome do usuário:");
+                    nomeDoUsuario = scanner.nextLine();
+                    System.out.println("Digite ID do usuário:");
+                    idCadastroDoUsuario = scanner.nextInt();
+                    usuario.setNome(nomeDoUsuario);
+                    usuario.setId(idCadastroDoUsuario);
+
+                    biblioteca.cadastrarUsuario(usuario);
+                    scanner.close();
+                    break;
+
+                case 3:
+                    //Realizar empréstimo de livro
+                    System.out.println("Digite o ISBN do livro:");
+                    isbnEmprestimo = scanner.nextLine();
+                    System.out.println("Digite o ID do usuario");
+                    idEmprestimoUsuario = scanner.nextInt();
+
+
+                    for (int i = 0; i < biblioteca.getLivros().size(); i++) {
+
+                        if (biblioteca.getLivros().get(i).getIsbn().equals(isbnEmprestimo)) {
+
+                            biblioteca.realizarEmprestimo(isbnEmprestimo, idEmprestimoUsuario);
+
+
+                        }
                     }
-                }
+
+                case 4:
+                    //Realiza devolução do livro
 
 
-
-            case 4:
-                //Realiza devolução do livro
-
-
-            case 5:
-                //Exibe livros disponíveis
+                case 5:
+                    //Exibe livros disponíveis
+                    System.out.println("Livros disponíveis:");
 
 
-            case 6:
-                System.exit(0);
+                case 6:
+                    System.exit(0);
 
 
-        }
+            }
+
+        } while (true);
+
     }
 }
+
+
 
 
